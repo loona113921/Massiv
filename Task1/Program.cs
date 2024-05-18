b448aee5983805e0bb1dd4a444f891023d97c9f7
Yuliya Ustimenko <loona113921@gmail.com>
2024-05-18 20:23:45 +0300

    Добавила решение к 1 заданию

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+void Main()
+{
+
+
+char[,] chars = 
+{
+    {'d','g','t','y'},
+    {'h','f','v','n'},
+    {'e','e','e','e'}
+};
+
+string str ="";
+foreach (char letter in chars)
+str += letter;
+System.Console.WriteLine(str);
+
+
+}
+Main();
