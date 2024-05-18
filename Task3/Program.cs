ee6cda0d79ffbb7e7c7a8d3fe97d526c4f69520f
Yuliya Ustimenko <loona113921@gmail.com>
2024-05-18 22:02:19 +0300

    Добавила решение к 3 заданию, не не полностью решила

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+void Main()
+{
+
+System.Console.WriteLine("Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+
+for(int i = 0; i<size;i++)
+{
+   
+char tmp = (char)rand.Next( 'A', 'Z' + 1 );
+System.Console.Write(tmp);
+
+}
+
+
+}
+Main();
